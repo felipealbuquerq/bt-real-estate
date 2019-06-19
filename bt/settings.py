175fe8969513b15cb50136a4ca75264451cc7348
Danilo Augusto <dasf_cm@terra.com.br>
2019-06-18 23:27:40 -0300

    chore: add postgresql db

--- bt/settings.py
+++ bt/settings.py
@@ -71,8 +71,11 @@ WSGI_APPLICATION = 'bt.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'btdb',
+        'USER': get_secret('DB_USER'),
+        'PASSWORD': get_secret('DB_PASSWORD'),
+        'HOST': 'localhost'
     }
 }
 
